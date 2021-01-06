723a2e6ed667d674a0961894b9a3507b6a3b5d45
chiragmahawar7 <cmahawar_be16@thapar.edu>
2021-01-06 21:44:51 +0530

    ngForm handling

--- src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
+++ src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { BudgetItem } from 'src/shared/models/budget-item.model';
 
 @Component({
   selector: 'app-budget-item-card',
@@ -7,10 +8,15 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class BudgetItemCardComponent implements OnInit {
 
-  @Input() isIncome: boolean = true;
+  @Input() item: BudgetItem;
+  @Output() xButtonClick: EventEmitter<any> = new EventEmitter<any>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onXButtonClick() {
+    this.xButtonClick.emit();
   }
 }
